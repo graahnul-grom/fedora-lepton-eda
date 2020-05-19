00e3d83493dfb6f777ba1a7bda84f9e8ba16990d
dmn <graahnul.grom@gmail.com>
2020-05-19 14:42:05 +0300

    run-rpmbuild: -c cmd line arg => run rpmlint

--- rpmbuild/run-rpmbuild.sh
+++ rpmbuild/run-rpmbuild.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-rpmlint SPECS/lepton-eda.spec
-if [ $? -ne 0 ]
+if [ "${1}" = "-c" ]
 then
-    echo -n " >> rpmlint errors. continue [y/N]? "
-    read answ
-    if [ "${answ}" != "y" ]
+    rpmlint --verbose SPECS/lepton-eda.spec
+    if [ $? -ne 0 ]
     then
-        exit 1
+        echo -n " >> rpmlint errors. continue [y/N]? "
+        read answ
+        if [ "${answ}" != "y" ]
+        then
+            exit 1
+        fi
     fi
 fi
 
