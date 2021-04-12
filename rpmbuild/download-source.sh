0e29065eb37dbbbb9499226ff32bb8b8787e6bcb
dmn <graahnul.grom@gmail.com>
2021-04-12 11:22:30 +0300

    update url in the download script

--- rpmbuild/download-source.sh
+++ rpmbuild/download-source.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-src="lepton-eda-1.9.13.tar.gz"
-url="https://github.com/lepton-eda/lepton-eda/releases/download/1.9.13-20201211/${src}"
+src="lepton-eda-1.9.14.tar.gz"
+url="https://github.com/lepton-eda/lepton-eda/releases/download/1.9.14-20210407/${src}"
 out="SOURCES/${src}"
 
 wget -c $url -O $out
