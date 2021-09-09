adecd1ac9a47d3456da9775db3f01f22a7952c12
dmn <graahnul.grom@gmail.com>
2021-09-09 20:43:45 +0300

    update to 1.9.16

--- rpmbuild/download-source.sh
+++ rpmbuild/download-source.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-src="lepton-eda-1.9.15.tar.gz"
-url="https://github.com/lepton-eda/lepton-eda/releases/download/1.9.15-20210626/${src}"
+src="lepton-eda-1.9.16.tar.gz"
+url="https://github.com/lepton-eda/lepton-eda/releases/download/1.9.16-20210731/${src}"
 out="SOURCES/${src}"
 
 wget -c $url -O $out
