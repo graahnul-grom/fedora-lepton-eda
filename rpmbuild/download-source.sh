38689a3f9fde7c7a5aa3adcae66a7d30d0ce0762
dmn <graahnul.grom@gmail.com>
2022-08-30 21:54:31 +0300

    download-source.sh: mkdir -p SOURCES

--- rpmbuild/download-source.sh
+++ rpmbuild/download-source.sh
@@ -4,5 +4,6 @@ src="lepton-eda-1.9.18.tar.gz"
 url="https://github.com/lepton-eda/lepton-eda/releases/download/1.9.18-20220529/${src}"
 out="SOURCES/${src}"
 
+mkdir -p SOURCES
 wget -c $url -O $out
 
